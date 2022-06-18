a13bebfc0acbbf8e7b8ec9537c91d446cca4c61e
DiegoPoke <107450706+DiegoPoke@users.noreply.github.com>
2022-06-18 20:49:25 -0300

    css da header

--- recipes/views.py
+++ recipes/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 
 def home(request):
     ...
-    return render(request, 'recipes/home.html', context={'name': 'diego poke'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'diego poke'})
 
 
 
